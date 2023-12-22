7548a02104e0315c74827d9a34d9170a36b8515d
PiriyaLapa <piriyalapa@gmail.com>
2023-12-23 01:37:12 +0700

    update qr-code-card2

--- src/Components/QrCodeCard2.tsx
+++ src/Components/QrCodeCard2.tsx
@@ -3,6 +3,7 @@ import {ImageQR, CardQR, ContentBox, QrBox } from "./qr-code-components";
 import styled from "styled-components";
 import ovalImg from "../img/OvalImage.png";
 import qrUrl from '../img/qr-code.png';
+import {data} from '../Data/data';
 const CardQR2 = styled(CardQR)`
   position: relative;
   margin: auto 0;
@@ -11,7 +12,23 @@ const CardQR2 = styled(CardQR)`
 const QrBox2 = styled(QrBox)`
     background-image:url(${ovalImg});
     background-size:cover;
-`
+`;
+interface ContentBoxProps {
+  topic:string,
+  content:string
+}
+
+class ContentBox2 extends Component<ContentBoxProps>{
+  render():ReactNode {
+    const {topic, content} = this.props;
+    return(
+      <ContentBox>
+        <h1>{topic}</h1>
+        <p>{content}</p>
+      </ContentBox>
+    )
+  }
+}
 
 export default class QrCodeCard2 extends Component {
   render(): ReactNode {
@@ -21,6 +38,7 @@ export default class QrCodeCard2 extends Component {
         <QrBox2>
             <ImageQR imgUrl={qrUrl} />
         </QrBox2>
+          <ContentBox2 topic={data.topic} content={data.content} />
         </CardQR2>
       </div>
     );
---
+++ src/Data/data.tsx
@@ -0,0 +1,5 @@
+
+export const data = {
+    topic: 'Improve your front-end skills by building projects',
+    content : 'Scan the QR code to visit Frontend Mentor and take your codi'
+}

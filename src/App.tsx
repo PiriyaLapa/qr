385bcdc6c577bb144031ec5b2ebdc8765a99fd5b
Piriya <piriyalapa@gmail.com>
2023-12-23 21:06:01 +0700

    Added shadow of QRCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Component, ReactNode } from "react";
 import { Container } from "./Components/qr-code-components";
-import QrCodeCard2 from './Components/QrCodeCard2';
+import QrCodeCard2 from "./Components/QrCodeCard2";
 
 export default class App extends Component {
   render(): ReactNode {
     return (
       <div>
         <Container>
-        <QrCodeCard2 /> 
+          <QrCodeCard2 />
+             {/* <p>Chalence by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">FrontendMentor</a> Coded by <a href="https://www.frontendmentor.io/profile/PiriyaLapa">WhoAmI</a></p> */}
         </Container>
       </div>
     );
--- src/Components/QrCodeCard2.tsx
+++ src/Components/QrCodeCard2.tsx
@@ -1,32 +1,32 @@
 import { Component, ReactNode } from "react";
-import {ImageQR, CardQR, ContentBox, QrBox } from "./qr-code-components";
+import { ImageQR, CardQR, ContentBox, QrBox } from "./qr-code-components";
 import styled from "styled-components";
 import ovalImg from "../img/OvalImage.png";
-import qrUrl from '../img/qr-code.png';
-import {data} from '../Data/data';
+import qrUrl from "../img/qr-code.png";
+import { data } from "../Data/data";
 const CardQR2 = styled(CardQR)`
   position: relative;
   margin: auto 0;
 `;
 
 const QrBox2 = styled(QrBox)`
-    background-image:url(${ovalImg});
-    background-size:cover;
+  background-image: url(${ovalImg});
+  background-size: cover;
 `;
 interface ContentBoxProps {
-  topic:string,
-  content:string
+  topic: string;
+  content: string;
 }
 
-class ContentBox2 extends Component<ContentBoxProps>{
-  render():ReactNode {
-    const {topic, content} = this.props;
-    return(
+class ContentBox2 extends Component<ContentBoxProps> {
+  render(): ReactNode {
+    const { topic, content } = this.props;
+    return (
       <ContentBox>
         <h1>{topic}</h1>
         <p>{content}</p>
       </ContentBox>
-    )
+    );
   }
 }
 
@@ -35,9 +35,9 @@ export default class QrCodeCard2 extends Component {
     return (
       <div>
         <CardQR2>
-        <QrBox2>
+          <QrBox2>
             <ImageQR imgUrl={qrUrl} />
-        </QrBox2>
+          </QrBox2>
           <ContentBox2 topic={data.topic} content={data.content} />
         </CardQR2>
       </div>

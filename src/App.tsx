458c2a4558db17ae31da6025163d8f21f1d94ec8
Piriya <piriyalapa@gmail.com>
2023-12-23 19:50:31 +0700

    update CardQR2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Component, ReactNode } from "react";
-// import QrCode from "./Components/qr-code-components";
 import { Container } from "./Components/qr-code-components";
 import QrCodeCard2 from './Components/QrCodeCard2';
+
 export default class App extends Component {
   render(): ReactNode {
     return (
